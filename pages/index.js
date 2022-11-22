35bcb21860a989f60e9911f48bf8bb8f2f9ee4d6
Thanapat01 <thanapat_hunter@hotmail.com>
2022-11-22 19:32:30 +0700

    fix bug

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,20 @@ export default function Component() {
   const { data: session } = useSession();
   const router = useRouter();
   if (session) {
-    router.push("/upload");
-
     return (
       <div className="flex flex-col justify-center items-center h-screen">
         <div className="text-xl">Signed in as {session.user.email}</div>
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-10"
-          onClick={() => signOut()}
+          onClick={() => router.push("api/auth/signout")}
         >
           Sign out
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-10"
+          onClick={() => router.push("/upload")}
+        >
+          Upload
         </button>
       </div>
     );
@@ -24,7 +28,7 @@ export default function Component() {
       <div className="text-xl">Not signed in</div>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-10"
-        onClick={() => signIn()}
+        onClick={() => router.push("api/auth/signin")}
       >
         Sign in
       </button>
